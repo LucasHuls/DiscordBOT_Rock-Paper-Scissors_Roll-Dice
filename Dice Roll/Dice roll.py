12a999f61c200005ad98dcf7afcccdd9baca1269
SemMtg <549452@student.saxion.nl>
2023-04-25 15:28:25 +0200

    Visual changes in dice roll and rock paper scissor

--- Dice Roll/Dice roll.py
+++ Dice Roll/Dice roll.py
@@ -1,6 +1,20 @@
 import discord, random
 from discord.ext import commands
 
+def convert_number_to_emoji(dice):
+    if dice == 1:
+        return ":one:"
+    elif dice == 2:
+        return ":two:"
+    elif dice == 3:
+        return ":three:"
+    elif dice == 4:
+        return ":four:"
+    elif dice == 5:
+        return ":five:"
+    elif dice == 6:
+        return ":six:"
+
 TOKEN = "MTEwMDAyNTIyNDA1MDc2OTk2MQ.GbF7tO.9EIEw6e7GSSujnmawdY-RqyJ9FXrgYkS-77Wss"
 
 
@@ -19,19 +33,20 @@ async def roll(ctx):
     print("Debug: Rolling dice.")
 
     dice_numbers = [1,2,3,4,5,6]
-    team_1_roll = random.choice(dice_numbers)
-    team_2_roll = random.choice(dice_numbers)
-    
+    team_2_roll = convert_number_to_emoji(random.choice(dice_numbers))
+    team_1_roll = convert_number_to_emoji(random.choice(dice_numbers))
+
+
     await ctx.channel.send(f"Team 1 rolled, {team_1_roll}")
 
     await ctx.channel.send(f"Team 2 rolled, {team_2_roll}")
     
     if team_1_roll == team_2_roll:
-        await ctx.channel.send("It's a Tie!")
+        await ctx.channel.send("It's a Tie! :yawning_face:")
     elif team_1_roll > team_2_roll:
-        await ctx.channel.send("Team 1 has Won!")
+        await ctx.channel.send("Team 1 has Won! :partying_face:")
     else:
-        await ctx.channel.send("Team 2 has Won!")
+        await ctx.channel.send("Team 2 has Won! :partying_face:")
 
 
 bot.run(TOKEN)
--- Rock-Paper-Scissors/Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors/Rock-Paper-Scissors.py
@@ -2,6 +2,13 @@
 import discord, random
 from discord.ext import commands
 
+def convert_string_to_emoji(prompt):
+    if prompt == "SCISSORS":
+        return ":scissors:"
+    elif prompt == "ROCK":
+        return ":rock:"
+    elif prompt == "PAPER":
+        return ":newspaper:"
 TOKEN = "MTEwMDAyNTIyNDA1MDc2OTk2MQ.GbF7tO.9EIEw6e7GSSujnmawdY-RqyJ9FXrgYkS-77Wss"
 
 
@@ -21,19 +28,20 @@ async def play(ctx, player_choice):
 
     options = ["ROCK", "PAPER", "SCISSORS"]
     
+
     if player_choice not in options:
         await ctx.channel.send("Invalid option!")
     else:
         bot_choice = random.choice(options)
         if bot_choice == player_choice:
-            await ctx.channel.send(f"It's a TIE... \nBOT has chosen: {bot_choice}     PLAYER chose: {player_choice}")
+            await ctx.channel.send(f"It's a TIE... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}     PLAYER chose: {convert_string_to_emoji(player_choice)}")
         elif bot_choice == "ROCK" and player_choice == "SCISSORS":
-            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {bot_choice}   PLAYER chose: {player_choice}")
+            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)}")
         elif bot_choice == "PAPER" and player_choice == "ROCK":
-            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {bot_choice}   PLAYER chose: {player_choice}")
+            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)}")
         elif bot_choice == "SCISSORS" and player_choice == "PAPER":
-            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {bot_choice}   PLAYER chose: {player_choice}")
+            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)}")
         else: 
-            await ctx.channel.send(f"Player Won!... \nBOT has chosen: {bot_choice}    PLAYER chose: {player_choice}")
-    
+            await ctx.channel.send(f"Player Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}    PLAYER chose: {convert_string_to_emoji(player_choice)}")
+
 bot.run(TOKEN)
