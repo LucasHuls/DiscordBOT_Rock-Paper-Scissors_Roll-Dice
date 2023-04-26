bcc444bf4295e4e323b9ab70e926dbf5ffd1b9f5
SemMtg <549452@student.saxion.nl>
2023-04-26 10:59:25 +0200

    Finished dice roll and rock paper scissors

--- Rock-Paper-Scissors/Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors/Rock-Paper-Scissors.py
@@ -34,14 +34,14 @@ async def play(ctx, player_choice):
     else:
         bot_choice = random.choice(options)
         if bot_choice == player_choice:
-            await ctx.channel.send(f"It's a TIE... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}     PLAYER chose: {convert_string_to_emoji(player_choice)}")
+            await ctx.channel.send(f"BOT has chosen: {convert_string_to_emoji(bot_choice)}     PLAYER chose: {convert_string_to_emoji(player_choice)} \nIt's a TIE...:yawning_face:")
         elif bot_choice == "ROCK" and player_choice == "SCISSORS":
-            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)}")
+            await ctx.channel.send(f"BOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)} \nBot Won!:robot:")
         elif bot_choice == "PAPER" and player_choice == "ROCK":
-            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)}")
+            await ctx.channel.send(f"BOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)} \nBot Won!:robot:")
         elif bot_choice == "SCISSORS" and player_choice == "PAPER":
-            await ctx.channel.send(f"Bot Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)}")
+            await ctx.channel.send(f"BOT has chosen: {convert_string_to_emoji(bot_choice)}   PLAYER chose: {convert_string_to_emoji(player_choice)} \nBot Won!:robot:")
         else: 
-            await ctx.channel.send(f"Player Won!... \nBOT has chosen: {convert_string_to_emoji(bot_choice)}    PLAYER chose: {convert_string_to_emoji(player_choice)}")
+            await ctx.channel.send(f"BOT has chosen: {convert_string_to_emoji(bot_choice)}    PLAYER chose: {convert_string_to_emoji(player_choice)} \n Player Won!:partying_face:")
 
 bot.run(TOKEN)
